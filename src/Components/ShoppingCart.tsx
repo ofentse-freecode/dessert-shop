28d441c9773eaa594e36f901d4831a73246aefb6
ofentse-freecode <ofentse.allphotos@gmail.com>
2024-08-27 17:05:25 +0200

    feat: total

--- src/Components/ShoppingCart.tsx
+++ src/Components/ShoppingCart.tsx
@@ -1,7 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { CartItem } from "./CartItem";
-
+import { formatCurrency } from "../utilities/formatCurrency";
+import storeItems from "../data/items.json"
 
 
 type ShoppingCartProps = {
@@ -26,6 +27,14 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
                 {...item} />
                 ))
                 }
+                <div className="ms-auto fw-bold fs-5">
+                 <h1>Total{" "} {formatCurrency(cartItems.reduce((total, cartItem) => {
+                    const item = storeItems.find(i => i.id === cartItem.id)
+                    return total + (item?.price || 0) * cartItem.quantity
+                  }, 0)
+                )}
+                </h1>
+                </div>
             </Stack>
         </Offcanvas.Body>
     </Offcanvas>
